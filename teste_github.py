7261fd503009701aed1a1a4e93a8a52923940c7c
Christian-31415 <120416342+Christian-31415@users.noreply.github.com>
2022-12-14 16:43:54 -0300

    Criado usando o Colaboratory

---
+++ teste_github.py
@@ -0,0 +1,54 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "provenance": [],
+      "authorship_tag": "ABX9TyOHfG8HSjylgZyPwjcqils6",
+      "include_colab_link": true
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    },
+    "language_info": {
+      "name": "python"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "markdown",
+      "metadata": {
+        "id": "view-in-github",
+        "colab_type": "text"
+      },
+      "source": [
+        "<a href=\"https://colab.research.google.com/github/Christian-31415/public_test/blob/teste_b/teste_github.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "execution_count": 1,
+      "metadata": {
+        "colab": {
+          "base_uri": "https://localhost:8080/"
+        },
+        "id": "9MhlAuQPmLeV",
+        "outputId": "3032b82a-73f1-43f8-f409-93c7ed697ea5"
+      },
+      "outputs": [
+        {
+          "output_type": "stream",
+          "name": "stdout",
+          "text": [
+            "hello\n"
+          ]
+        }
+      ],
+      "source": [
+        "def return_hello():\n",
+        "  return 'hello'"
+      ]
+    }
+  ]
+}
